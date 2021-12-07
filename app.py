e111c881b8c71b95723ca4371d8c146b05f72365
minseongkimdev <yssj2049@gmail.com>
2021-12-07 13:39:52 +0900

    test

--- app.py
+++ app.py
@@ -40,6 +40,10 @@ pkle.dump(new_choice.index(choice), open('next.p', 'wb'))
 # finally get to whats on each page
 if choice == 'Home':
     st.write('this is home')
+    st.radio(
+            "",
+            ('원하는 서비스를 선택해주세요', '건강을 챙기는 으르신', '먹고죽자 치팅데이', '비건에의한 비건을위한', '밤에 출출한 야식러',
+             '생각없는 당신을위한 랜덤', '선택장애를 위한 월드컵'))
 elif choice == 'Resources':
     st.write('here is a resources page')
 elif choice == 'Gallery':
